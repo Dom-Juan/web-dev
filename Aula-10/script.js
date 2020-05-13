d8d9b402f85aa294ec40c5d418ae108476c77e94
Juan Cardoso da Silva <35587574+Dom-Juan@users.noreply.github.com>
2020-05-13 18:23:01 -0300

    Adicionado exercícios
    Adicionado exercício da aula 10.

---
+++ Aula-10/script.js
@@ -0,0 +1,15 @@
+let numeroDeFilhos = 20;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function AdicionaConteudo(){
+    i = 0;
+    
+    while(i < numeroDeFilhos) {
+        await sleep(1000);
+        document.getElementById('coisas').innerHTML+=`<div class='postBox'><textarea class="postBox" name="text" rows="5" cols="33">Bottom Texto</textarea></div>`;
+        i++;
+    }
+}

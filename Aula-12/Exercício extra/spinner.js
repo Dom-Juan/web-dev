f1a7a4e36218afb97be76401a022870b6a1d5190
Juan Cardoso da Silva <35587574+Dom-Juan@users.noreply.github.com>
2020-05-29 17:56:28 -0300

    Adicionado aula 12
    Exercício sobre responsive menu e um exercício extra de animação.

---
+++ Aula-12/Exercício extra/spinner.js
@@ -0,0 +1,4 @@
+function iniciaSpin() {
+    console.log("entrou\n");
+    document.getElementById('t').classList.add('spinner');
+}
---
+++ Aula-12/hamburger.js
@@ -0,0 +1,9 @@
+function abrirSlideMenu(){
+    document.getElementById('mobile-nav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+}
+
+function fecharSlideMenu(){
+    document.getElementById('mobile-nav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+}
